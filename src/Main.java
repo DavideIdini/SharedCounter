4eb70e586d098de7552d62536a840af02525b692
Davide <davide76.di@gmail.com>
2023-01-28 18:53:15 +0100

    bug irrisolto

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,44 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
     SharedCounter uno = new SharedCounter(10);
         SharedCounter due = new SharedCounter(20);
         SharedCounter tre = new SharedCounter(10);
         SharedCounter quattro = new SharedCounter(0);
     uno.start();
     due.start();
-    tre.start();quattro.start();
+    tre.start();
+    quattro.start();
+    uno.join();
+    due.join();
+    tre.join();
+    quattro.join();
 
     }
 
 }
 
 
-class SharedCounter extends Thread{
+class SharedCounter extends Thread {
     int contatore = 0;
     int n;
 
     boolean libero = true;
-    synchronized public int incr(int n){
-        while(contatore==n)
-        contatore++;
+
+    synchronized public int incr(int n) {
+        while (contatore == n)
+            contatore++;
         int tmp = contatore;
-        System.out.println("sono arrivato incrementando a "+ tmp);
+        System.out.println("sono arrivato incrementando a " + tmp);
         libero = true;
         notify();
         return tmp;
     }
-    synchronized public int decr(int n){
-        while(contatore==n)
-        contatore--;
+
+    synchronized public int decr(int n) {
+        while (contatore == n)
+            contatore--;
         int tmp = contatore;
-        System.out.println("sono arrivato decrementanto a "+ tmp);
+        System.out.println("sono arrivato decrementanto a " + tmp);
         libero = true;
         notifyAll();
         return tmp;
@@ -46,22 +53,28 @@ class SharedCounter extends Thread{
     @Override
     public void run() {
         super.run();
-        while(true) {
-            if (libero) {
-                libero = false;
+        while (true) {
 
-            if (n >= contatore)
-                decr(n);
-            else
-                incr(n);
-        }
-        else{
+            if (!libero) {
                 try {
                     wait();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+            } else {
+                libero = false;
+
+                if (n >= contatore) {
+                    decr(n);
+                    return;
+                }
+                else{
+                    incr(n);
+                    return;
+                }
+
             }
+
         }
     }
 }
