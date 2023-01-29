d95b7d0b80022c4a38f20b2fe843fbc447991ce4
Davide <davide76.di@gmail.com>
2023-01-29 20:21:14 +0100

    nuova versione

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,111 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         SharedCounter sharedCounter = new SharedCounter();
-        Thread t1 = new Thread(){
+        Thread t1 = new Thread() {
             int n = 10;
-            public void run(){
-                n = sharedCounter.waitForValue(n);
-            }
+
+            public synchronized void run() {
+                if (!sharedCounter.isLibero()) {
+                    try {
+                        System.out.println("sono in attesa per "+ n);
+                        wait();
+                        System.out.println("sono stato svegliato");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                    sharedCounter.setLibero(false);
+                    if (n == sharedCounter.getCounter()) {
+                        System.out.println("sono gia a " + n);
+                    }
+                    if (n > sharedCounter.getCounter())
+                        n = sharedCounter.incr(n);
+                    else if (n < sharedCounter.getCounter()) {
+                        n = sharedCounter.decr(n);
+                    }
+                    sharedCounter.setLibero(true);
+                    notifyAll();
+                }
+
+
         };
-        Thread t2 = new Thread(){
-            int n = 5;
-            public void run(){
-                n = sharedCounter.waitForValue(n);
-            }
+        Thread t2 = new Thread() {
+            int n = 50;
+
+            public synchronized void run() {
+                    if (!sharedCounter.isLibero()) {
+                            try {
+                                System.out.println("sono in attesa per "+ n);
+                                wait();
+                                System.out.println("sono stato svegliato");
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+
+
+
+                    sharedCounter.setLibero(false);
+                    if (n == sharedCounter.getCounter()) {
+                        System.out.println("sono gia a " + n);
+                    }
+                    if (n > sharedCounter.getCounter())
+                        n = sharedCounter.incr(n);
+                    else if (n < sharedCounter.getCounter()) {
+                        n = sharedCounter.decr(n);
+                    }
+                    sharedCounter.setLibero(true);
+                    notifyAll();
+                }
+
+
         };
-        t1.start();
+        t1.start();t1.join();
         t2.start();
 
-    }
 
+    }
 }
-class SharedCounter{
-    private static int counter = 0;
-    private boolean libero = true;
 
-    public synchronized static int getCounter() {
-        return counter;
-    }
+class SharedCounter {
+        private static int counter = 0;
+        private boolean libero = true;
 
-    public synchronized static void setCounter(int counter) {
-        SharedCounter.counter = counter;
-    }
-    public synchronized int incr(int n){
-        System.out.println(" incr counter "+ counter+" n "+ n);
-        while(counter<n)
-            counter++;
-        System.out.println("sono arrivato a "+ counter);
-        return n;
-    }
-    public synchronized int decr(int n){
-        System.out.println("decr counter "+ counter+" n "+ n);
-        while(counter>n){
-            counter--;
+        public boolean isLibero() {
+            return libero;
         }
-        System.out.println("sono arrivato a "+ counter);
-        return n;
-    }
-    public synchronized int  waitForValue(int n){
-        if(libero==false) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
+        public void setLibero(boolean libero) {
+            this.libero = libero;
         }
-        libero = false;
-        if(n==counter)
+
+        public synchronized static int getCounter() {
+            return counter;
+        }
+
+        public synchronized static void setCounter(int counter) {
+            SharedCounter.counter = counter;
+        }
+
+        public synchronized int incr(int n) {
+            System.out.println(" incr counter " + counter + " n " + n);
+            while (counter < n)
+                counter++;
+            System.out.println("sono arrivato a " + counter);
             return n;
-        if(n>counter)
-            n=incr(n);
-        else if (n<counter) {
-            n=decr(n);
         }
-        libero = true;
-        notify();
-        return n;
+
+        public synchronized int decr(int n) {
+            System.out.println("decr counter " + counter + " n " + n);
+            while (counter > n) {
+                counter--;
+            }
+            System.out.println("sono arrivato a " + counter);
+            return n;
+        }
+
+
     }
-}
+
 
 
