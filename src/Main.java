3dd499e31fd75e15be2c89fc9e81d1700d702935
Davide <davide76.di@gmail.com>
2023-01-29 14:28:07 +0100

    bug risolto(?)

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,70 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-    SharedCounter uno = new SharedCounter(10);
-        SharedCounter due = new SharedCounter(20);
-        SharedCounter tre = new SharedCounter(10);
-        SharedCounter quattro = new SharedCounter(0);
-    uno.start();
-    due.start();
-    tre.start();
-    quattro.start();
-    uno.join();
-    due.join();
-    tre.join();
-    quattro.join();
+        SharedCounter sharedCounter = new SharedCounter();
+        Thread t1 = new Thread(){
+            int n = 10;
+            public void run(){
+                n = sharedCounter.waitForValue(n);
+            }
+        };
+        Thread t2 = new Thread(){
+            int n = 5;
+            public void run(){
+                n = sharedCounter.waitForValue(n);
+            }
+        };
+        t1.start();
+        t2.start();
 
     }
 
 }
+class SharedCounter{
+    private static int counter = 0;
+    private boolean libero = true;
 
+    public synchronized static int getCounter() {
+        return counter;
+    }
 
-class SharedCounter extends Thread {
-    int contatore = 0;
-    int n;
-
-    boolean libero = true;
-
-    synchronized public int incr(int n) {
-        while (contatore == n)
-            contatore++;
-        int tmp = contatore;
-        System.out.println("sono arrivato incrementando a " + tmp);
+    public synchronized static void setCounter(int counter) {
+        SharedCounter.counter = counter;
+    }
+    public synchronized int incr(int n){
+        System.out.println(" incr counter "+ counter+" n "+ n);
+        while(counter<n)
+            counter++;
+        System.out.println("sono arrivato a "+ counter);
+        return n;
+    }
+    public synchronized int decr(int n){
+        System.out.println("decr counter "+ counter+" n "+ n);
+        while(counter>n){
+            counter--;
+        }
+        System.out.println("sono arrivato a "+ counter);
+        return n;
+    }
+    public synchronized int  waitForValue(int n){
+        if(libero==false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        libero = false;
+        if(n==counter)
+            return n;
+        if(n>counter)
+            n=incr(n);
+        else if (n<counter) {
+            n=decr(n);
+        }
         libero = true;
         notify();
-        return tmp;
-    }
-
-    synchronized public int decr(int n) {
-        while (contatore == n)
-            contatore--;
-        int tmp = contatore;
-        System.out.println("sono arrivato decrementanto a " + tmp);
-        libero = true;
-        notifyAll();
-        return tmp;
-
-    }
-
-
-    public SharedCounter(int n) {
-        this.n = n;
-    }
-
-    @Override
-    public void run() {
-        super.run();
-        while (true) {
-
-            if (!libero) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                libero = false;
-
-                if (n >= contatore) {
-                    decr(n);
-                    return;
-                }
-                else{
-                    incr(n);
-                    return;
-                }
-
-            }
-
-        }
+        return n;
     }
 }
+
+
