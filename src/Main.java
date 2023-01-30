ab94303e400749fb9e427f506d6baefda82014bf
Davide <davide76.di@gmail.com>
2023-01-30 16:27:52 +0100

    nuova versione

--- src/Main.java
+++ src/Main.java
@@ -1,111 +1,77 @@
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        SharedCounter sharedCounter = new SharedCounter();
-        Thread t1 = new Thread() {
-            int n = 10;
-
-            public synchronized void run() {
-                if (!sharedCounter.isLibero()) {
-                    try {
-                        System.out.println("sono in attesa per "+ n);
-                        wait();
-                        System.out.println("sono stato svegliato");
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                    sharedCounter.setLibero(false);
-                    if (n == sharedCounter.getCounter()) {
-                        System.out.println("sono gia a " + n);
-                    }
-                    if (n > sharedCounter.getCounter())
-                        n = sharedCounter.incr(n);
-                    else if (n < sharedCounter.getCounter()) {
-                        n = sharedCounter.decr(n);
-                    }
-                    sharedCounter.setLibero(true);
-                    notifyAll();
-                }
-
-
-        };
-        Thread t2 = new Thread() {
-            int n = 50;
-
-            public synchronized void run() {
-                    if (!sharedCounter.isLibero()) {
-                            try {
-                                System.out.println("sono in attesa per "+ n);
-                                wait();
-                                System.out.println("sono stato svegliato");
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-
-
-
-                    sharedCounter.setLibero(false);
-                    if (n == sharedCounter.getCounter()) {
-                        System.out.println("sono gia a " + n);
-                    }
-                    if (n > sharedCounter.getCounter())
-                        n = sharedCounter.incr(n);
-                    else if (n < sharedCounter.getCounter()) {
-                        n = sharedCounter.decr(n);
-                    }
-                    sharedCounter.setLibero(true);
-                    notifyAll();
-                }
-
-
-        };
-        t1.start();t1.join();
+class Main{
+    public static void main(String[]args){
+        SharedCounter counter = new SharedCounter();
+        Mythreads t1 = new Mythreads(10, counter);
+        Mythreads t2 = new Mythreads(20, counter);
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         t2.start();
-
 
     }
 }
 
-class SharedCounter {
-        private static int counter = 0;
-        private boolean libero = true;
+ class SharedCounter {
+    private int counter =0;
+    boolean libero = true;
 
-        public boolean isLibero() {
-            return libero;
-        }
+     public int getCounter() {
+         return counter;
+     }
+     public boolean isLibero() {
+         return libero;
+     }
 
-        public void setLibero(boolean libero) {
-            this.libero = libero;
-        }
+     public void setLibero(boolean libero) {
+         this.libero = libero;
+     }
 
-        public synchronized static int getCounter() {
-            return counter;
-        }
+     public synchronized void incrementa()
+    { counter ++; }
+    public synchronized void decrementa()
+    { counter--; }
+}
+class Mythreads extends Thread{
+    int n;
+    SharedCounter sc;
 
-        public synchronized static void setCounter(int counter) {
-            SharedCounter.counter = counter;
-        }
-
-        public synchronized int incr(int n) {
-            System.out.println(" incr counter " + counter + " n " + n);
-            while (counter < n)
-                counter++;
-            System.out.println("sono arrivato a " + counter);
-            return n;
-        }
-
-        public synchronized int decr(int n) {
-            System.out.println("decr counter " + counter + " n " + n);
-            while (counter > n) {
-                counter--;
-            }
-            System.out.println("sono arrivato a " + counter);
-            return n;
-        }
-
-
+    public Mythreads(int n, SharedCounter counter) {
+        this.n = n;
+        this.sc = counter;
     }
+
+    @Override
+    public void run() {
+        super.run();
+        if(!sc.isLibero()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        sc.setLibero(false);
+            if(n< sc.getCounter()){
+                while(n<sc.getCounter())
+                    sc.decrementa();
+                System.out.println("sono arrivato a "+sc.getCounter());
+            } else if (n> sc.getCounter()) {
+                while(n>sc.getCounter())
+                    sc.incrementa();
+                System.out.println("sono arrivato a "+sc.getCounter());
+            } else if (n== sc.getCounter()) {
+                System.out.println("sono già a "+sc.getCounter());
+            }
+            sc.setLibero(true);
+            notify();
+
+
+            }
+
+        }
 
 
 
